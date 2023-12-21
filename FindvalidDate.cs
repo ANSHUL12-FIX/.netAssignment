1744c30a511478b4e366306c680bfe1d3ad659d2
ANSHUL12-FIX <64637596+ANSHUL12-FIX@users.noreply.github.com>
2023-12-21 11:41:24 +0530

    Update and rename FindvalidDate to FindvalidDate.cs

---
+++ FindvalidDate.cs
@@ -0,0 +1,28 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_date_from_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hdjsh asd2324234jghjsd hjsdg sdhk 12212021 idf32432 32423 d34234jh dfh";
+            string[] words = input.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (word.Length == 8 && int.TryParse(word, out _))
+                {
+                    Console.WriteLine(word);
+                    break;
+                }
+            }
+
+        }
+    }
+}
